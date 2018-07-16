eef79aaac55efb3e483f6cdc8cf3c22dcc8c119d
马玉谋 <mayumou@wintelia.com>
2018-07-16 09:22:12 +0800

    删除无用jar包

--- src/main/java/com/seeMovie/controller/MainPageController.java
+++ src/main/java/com/seeMovie/controller/MainPageController.java
@@ -1,19 +1,13 @@
 package com.seeMovie.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import com.seeMovie.common.Page;
-import com.seeMovie.common.PageParserTool;
-import com.seeMovie.common.RequestAndResponseTool;
-import com.seeMovie.common.link.LinkFilter;
 import com.seeMovie.common.utils.PagingUtil;
 import com.seeMovie.pojo.MovieVo;
 import com.seeMovie.service.TestService;
-
 /**
  * 
  * @author mym
