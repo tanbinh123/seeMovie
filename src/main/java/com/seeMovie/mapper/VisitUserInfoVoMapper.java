f0563505e030e3410a02ab7e7cc5c577bb0ddbf6
mayumou <mayumou@wintelia.com>
2018-08-24 22:07:01 +0800

    buchong

---
+++ src/main/java/com/seeMovie/mapper/VisitUserInfoVoMapper.java
@@ -0,0 +1,16 @@
+package com.seeMovie.mapper;
+
+import com.seeMovie.pojo.VisitUserInfoVo;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface VisitUserInfoVoMapper {
+    int insert(VisitUserInfoVo record);
+
+    List<VisitUserInfoVo> selectAll();
+    //根据用户名查询最近十次登录信息
+    List<VisitUserInfoVo> selectTheLastTenVisitsByUserName(@Param("userName") String userName);
+    //根据用户名查询所有登录次数
+    int selectAllVisitNumsByUserName(@Param("userName")String userName);
+}
